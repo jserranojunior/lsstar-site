01d6accf30be21d1168fa5b6d2adacdafd0bf111
Jorge Serrano Junior <jorgeserarnojunior@hotmail.com>
2018-08-09 09:44:31 -0300

    Novo Site

---
+++ novo/footer.php
@@ -0,0 +1,39 @@
+<div class="footer">
+<div class="row">
+        <div class="col  ">
+        <p class="text-center bg-light">
+                Av. dos Caiçaras, 31 - Jardim Guaiúba - Guarujá | Tel: (11) 99971-2869                
+        </p>
+         
+        </div>
+      </div>
+</div>
+
+
+</div>    <!-- Container -->
+    <style>
+
+.icon-social-media-header {
+    font-size: 2.5rem;
+}
+
+      .row-space{
+      margin-top:5px;
+      }
+      .sombra {
+      box-shadow: 0 1px 10px rgba(0,0,0,0.19), 0 2px 2px rgba(0,0,0,0.23);
+      }
+
+      .sidebar-left {
+    padding-left: 24px;
+}
+
+      .navbar{
+        padding: .0rem 1rem;
+        }
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
---
+++ novo/index.php
@@ -0,0 +1,93 @@
+<?php 
+    include('header.php');
+?>
+      <div class="row row-space">
+    <div class="col">
+       <div class="row">
+        <div class="col">
+        <div class="sidebar-left">
+            <a class=" text-dark" href="#">                    
+                <p>
+                Contato
+                </p>                     
+            </a>
+            <a class=" text-dark" href="#">                    
+                <p>
+                Quem somos
+                </p>                     
+            </a>
+            <a class=" text-dark" href="#">                    
+                <p>
+                Projetos Realizados
+                </p>                     
+            </a>
+            <a class=" text-dark" href="#">                    
+                <p>
+                Projetos em Obras
+                </p>                     
+            </a>
+            <a class=" text-dark" href="#">                    
+                <p>
+                Projetos Futuros
+                </p>                     
+            </a>
+            </div>
+        </div>
+        
+       </div>
+        
+       
+
+    </div>
+
+        <div class="col-lg-6">
+          <div class="row">
+            <div class="col col-logo">
+              <img src="img/logo.png " class="img-fluid mx-auto d-block sombra" alt="Ls Star Incorporadora">
+            </div>
+          </div>
+          <div class="row float-center row-space">
+            <div class="col-md">
+              <img src="img/casa.jpg" class="img-fluid mx-auto d-block" alt="Ls Star Incorporadora">
+            </div>
+          </div>
+          <!--
+          <div class="row row-space">
+            <div class="col-md-4">
+              <div class="card">
+                <img class="card-img-top img-fluid" src="./img/asturias-slide.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text text-center">Arilene Farinazzo</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <img class="card-img-top img-fluid" src="./img/asturias-slide.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text text-center">Arilene Farinazzo</p>
+                </div>
+              </div>
+            </div>
+            <div class="col-md-4">
+              <div class="card">
+                <img class="card-img-top img-fluid" src="./img/asturias-slide.jpg" alt="Card image cap">
+                <div class="card-body">
+                  <p class="card-text text-center">Arilene Farinazzo</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          -->
+        </div>
+        
+        <div class="col-lg-3">
+          <div class="widget plugin-facebook">
+            <!-- <h4 class="sidebar-title text-center">Facebook</h4> -->
+            <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Flsstarincorporadora%2F&tabs=timeline&width=250&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId" width="250" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+          </div>
+        </div>
+      </div>
+      <?php 
+    include('footer.php');
+?>
