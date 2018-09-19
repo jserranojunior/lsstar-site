acb66d90c7c07aac253fe85ca5cd6fadff94ffff
Jorge Serrano Junior <jorgeserarnojunior@hotmail.com>
2018-09-19 16:25:39 -0300

    Video com música e na tela inicial

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,46 +4,57 @@
 @section('content')
 
 
-  <div class="row row-lsstar">
-    <div class="col col-logo">
-      <img src="img/logo.png " class="img-fluid mx-auto d-block sombra" alt="Ls Star Incorporadora">
-    </div>
+<div class="row row-lsstar">
+  <div class="col col-logo">
+    <img src="img/logo.png " class="img-fluid mx-auto d-block sombra" alt="Ls Star Incorporadora">
   </div>
-  <div class="row float-center row-casa">
-    <div class="col-md">
-      <img src="img/casa.jpg" class="img-fluid mx-auto d-block" alt="Ls Star Incorporadora">
-    </div>
+</div>
+<div class="row float-center row-casa">
+  <div class="col-md">
+    <img src="img/casa.jpg" class="img-fluid mx-auto d-block" alt="Ls Star Incorporadora">
   </div>
-  <div class="row row-financiamento justify-content-md-center">
-    <div class="col-md-6">
-      <span class="font-weight-bold text-center">
-        <p class="text-center">
-          <span class="font-weight-bold text-center">
-            Imóveis a Venda
-            <br>
-          </span>
-          <a class="" href="{{url('/adriano')}}">
+</div>
+<div class="row row-financiamento justify-content-md-center">
+  <div class="col-md-6">
+    <span class="font-weight-bold text-center">
+      <p class="text-center">
+        <span class="font-weight-bold text-center">
+          Imóveis a Venda
+          <br>
+        </span>
+        <a class="" href="{{url('/adriano')}}">
           Adriano Dias
-            <br> Casa 3</a>
-          <br>
-          <br>
-          <a class="" href="{{url('/arilene')}}">
+          <br> Casa 3</a>
+        <br>
+        <br>
+        <a class="" href="{{url('/arilene')}}">
           Arilene Farinazzo
-            <br> Casa 3 e 4</a>
-        </p>
-
-    </div>
-    <div class="col-md-6 mx-auto d-block text-center">
-      <span class="font-weight-bold">
+          <br> Casa 3 e 4</a> <br><br>
         <a target="_blank" href="http://www8.caixa.gov.br/siopiinternet-web/simulaOperacaoInternet.do?method=inicializarCasoUso">
-          Simulador de Financiamento
+          Simulador de Financiamento - <img src="img/minhacasaminhavida.png" class="img-fluid img-minha-casa-minha-vida img-thumbnail mx-auto d-block" alt="Minha casa minha vida">
         </a>
-      </span>
+      </p>
 
-      <img src="img/minhacasaminhavida.png" class="img-fluid img-minha-casa-minha-vida img-thumbnail mx-auto d-block" alt="Minha casa minha vida">
-      </a>
-    </div>
   </div>
+  <div class="col-md-6 mx-auto d-block text-center">
+      <span class="font-weight-bold text-center"> Jardin Las Palmas <br> Lançamento em Outubro </span>
+    
+     <div class="col">
+        <video class="black" id="meuvideo" width="100%" height="100%" type="video/mp4" loop controls autoplay>
+          <source src="{{url('/video/video-externo-e-interno.mp4')}}"> Seu navegador não tem suporte.
+        </video>
+     </div>
+
+    <!-- <span class="font-weight-bold">
+      <a target="_blank" href="http://www8.caixa.gov.br/siopiinternet-web/simulaOperacaoInternet.do?method=inicializarCasoUso">
+        Simulador de Financiamento
+      </a>
+    </span>
+    <img src="img/minhacasaminhavida.png" class="img-fluid img-minha-casa-minha-vida img-thumbnail mx-auto d-block" alt="Minha casa minha vida">
+    </a> -->
+ 
+  </div>
+</div>
 
 
 @endsection
