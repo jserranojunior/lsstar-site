bf0a0100fe6621ccbc63b24df1b88589c9bc569b
Jorge S Junior <jorgeserranojunior@hotmail.com>
2021-03-08 16:45:51 -0300

    video

--- vue/src/modules/institucional/use/useState.js
+++ vue/src/modules/institucional/use/useState.js
@@ -7,7 +7,7 @@ export const useState = () => {
     logo: require("@/assets/imgs/logo.png"),
     banner: require("@/assets/imgs/institucional/banner-home.png"),
 
-    video: "@/assets/videos/videolaspalmas.mp4",
+    video: require("../../../../public/videos/videolaspalmas.mp4"),
     linkmapa:
       "https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d1288.6079629238022!2d-46.274691206548184!3d-24.005178528126653!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spt-BR!2sbr!4v1613755467581!5m2!1spt-BR!2sbr",
   });
